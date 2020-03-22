b40d8e1d481b094dd0e903de0dcd02baf1f94be9
Paolo <paolo.racedb@gmail.com>
2020-03-22 21:39:35 +0800

    started converting to .js

---
+++ new/src/App.js
@@ -0,0 +1,22 @@
+import React,{Component} from 'react';
+import {BrowserRouter, Route} from 'react-router-dom';
+
+import Homepage from "./pages/Homepage";
+import AddEvent from "./pages/AddEvent";
+import Archive from "./pages/Archive";
+
+export default class App extends Component{
+  render(){
+    return (
+      <div>
+        <BrowserRouter>
+          <div>
+            <Route exact={true} path="/" component={Homepage} />
+            <Route exact={true} path="/add-event" component={AddEvent}/>
+			<Route exact={true} path="/archive" component={Archive}/>
+          </div>
+        </BrowserRouter>
+      </div>
+    );
+  }
+}
